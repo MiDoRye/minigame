82d32fcc4546214515aa70e1490b65e598789a30
Lee DoYoon <ehdbs1276@naver.com>
2023-03-28 10:41:54 +0900

    class Warrior add
    class warrior add
    In Character class, attacked_by_other function has added
    This function check others attack and attack_dmg, and change self.hp and
    self.barrier(warrior)

--- Character.py
+++ Character.py
@@ -8,25 +8,31 @@ class Character:
         self.hp = hp
         self.power = power
         self.mp = 20
+        self.attack_dmg = 0
 
     def attack(self, other):
-        damage = random.randint(
-            self.power - self.power/10, self.power + self.power/10)
-        other.hp = max(other.hp - damage, 0)
-        print(f"{self.name}의 공격! {other.name}에게 {damage}의 데미지를 입혔습니다.")
+        self.attack_dmg = self.power
+        if random.randint(1, 2) > 1:
+            self.attack_dmg = self.attack_dmg*2
+        print(f"{self.name}의 공격!")
+        other.attacked_by_other(self)
 
     def magic(self, other):
-        damage = random.randint(self.mp - 5, self.mp + 5)
-        other.hp = max(other.hp - damage, 0)
-        print(f"{self.name}의 마법! {other.name}에게 {damage}의 데미지를 입혔습니다.")
+        self.attack_dmg = random.randint(self.mp - 5, self.mp + 5)
+        print(f"{self.name}의 마법!")
+        other.attacked_by_other(self)
 
     def magic_power(self):
         add_mp = random.randint(0, 10)
         self.mp += add_mp
         print(f"{self.name}의 마법 파워가 {add_mp}만큼 증가하였습니다.")
 
+    def attacked_by_other(self, other):
+        self.hp = self.hp - other.attack_dmg
+        print(f"{self.name}에게 {other.attack_dmg}의 데미지를 입혔습니다.")
+
     def alive(self):
-        if self.hp == 0:
+        if self.hp <= 0:
             print(f"{self.name}이(가) 죽었습니다.")
             return False
 
@@ -34,26 +40,35 @@ class Character:
         print(f"{self.name}의 상태: HP {self.hp}/{self.max_hp}")
 
 
-# class Warrior(Character):
-#     def __init__(self, name):
-#         self.name = name
-#         self.max_hp = 150
-#         self.hp = 150
-#         self.barrier = 20
-#         self.power = 15
+class Warrior(Character):
+    def __init__(self, name):
+        self.name = name
+        self.max_hp = 150
+        self.hp = 150
+        self.barrier = 10
+        self.power = 15
 
-#     def barrier(self):
-#         self.barrier += 10
-#         print(f"{self.name}의 베리어가 10만큼 증가하였습니다.")
+    def inc_barrier(self):
+        self.barrier += 10
+        print(f"{self.name}의 베리어가 10만큼 증가하였습니다.")
 
-#     def strong_attack(self, other):
-#         damage = random.randint(self.power + self.barrier,
-#                                 self.power + self.barrier + 5)
-#         if random.randint(1, 2) > 1:
-#             damage = damage*2
-#         other.hp = max(other.hp - damage, 0)
-#         print(f"{self.name}의 공격! {other.name}에게 {damage}의 데미지를 입혔습니다.")
+    def strong_attack(self, other):
+        self.attack_dmg = self.barrier
+        if random.randint(1, 2) > 1:
+            self.attack_dmg = self.attack_dmg*2
+        print(f"{self.name}의 강공격!")
+        other.attacked_by_other(self)
 
-#     def show_status(self):
-#         print(f"{self.name}의 상태: HP {self.hp}/{self.max_hp}")
-#         print(f"{self.name}의 베리어: barrier {self.barrier}")
+    def show_status(self):
+        print(
+            f"{self.name}의 상태: HP {self.hp}/{self.max_hp}\tbarrier {self.barrier}")
+
+    def attacked_by_other(self, other):
+        if self.barrier > 0:
+            self.barrier = self.barrier - other.attack_dmg
+
+        if self.barrier < 0:
+            self.hp = self.hp + self.barrier
+            self.barrier = 0
+
+        print(f"{self.name}에게 {other.attack_dmg}의 데미지를 입혔습니다.")
